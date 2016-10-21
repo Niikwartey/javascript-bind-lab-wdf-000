709f6cd23f2a3f3fd25a5b249b564687c33406c0
Derick Quartey <niikwartey1@gmail.com>
2016-10-21 11:38:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+var digitalClock = {
+  time: Math.round( Date.now() / 1000 ),
+  startTicking() {
+    this.time += 1
+    window.setInterval(this.startTicking, 1000)
+  }
+}
+
+const censor = (badWord, sentence) => {
+  return sentence.split(badWord).join('BLEEP')
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');
